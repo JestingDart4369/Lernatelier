df6d7478cf423b8327ab54d137e94a806a9fd939
JestingDart4369 <eric.haeusler@gmx.ch>
2025-08-24 11:02:41 +0200

    Tag2

--- 02_SpileSammlung/Gänse Spiel/Testing/Program.cs
+++ 02_SpileSammlung/Gänse Spiel/Testing/Program.cs
@@ -1,6 +1,3 @@
-﻿using System.Security.Cryptography;
-Console.ReadLine();
-for (int i = 0;i < 32; i++)
-{
-    Console.WriteLine(1 + RandomNumberGenerator.GetInt32(6));
-}
+﻿using Testing;
+
+Testing.Test;
--- 02_SpileSammlung/Gänse Spiel/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
+++ 02_SpileSammlung/Gänse Spiel/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b01fb69863060b0c8a1b7a1ca3b36e2cfe1bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e1ffe3f8892a6d8e813a13e5a883b73c49d0c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
