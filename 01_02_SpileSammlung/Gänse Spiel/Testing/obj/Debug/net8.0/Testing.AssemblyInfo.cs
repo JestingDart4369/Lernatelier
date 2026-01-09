34a7708f22580f503de63dc9f4d9f380bcfa53a9
JestingDart4369 <eric.haeusler@gmx.ch>
2026-01-09 07:42:29 +0100

    asd

--- 01_02_SpileSammlung/Gänse Spiel/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
+++ 01_02_SpileSammlung/Gänse Spiel/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea85ceff4112cfb722f6c9c0cdca5cba0c42545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1bef52884683f43e31e0784f4a949008799a74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
