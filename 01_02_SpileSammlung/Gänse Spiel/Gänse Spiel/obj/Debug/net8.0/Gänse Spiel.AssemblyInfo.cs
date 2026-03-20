7e6066a3665bbac0ab492b9dcc634f05c8f977a2
JestingDart4369 <eric.haeusler@gmx.ch>
2026-03-20 07:36:45 +0100

    Improves Obsidian vault organization and productivity
    Enhances Obsidian vault functionality by expanding its capacity, filling it with relevant data, and streamlining its structure. This change aims to improve overall note-taking efficiency and reduce clutter.

--- 01_02_SpileSammlung/Gänse Spiel/Gänse Spiel/obj/Debug/net8.0/Gänse Spiel.AssemblyInfo.cs
+++ 01_02_SpileSammlung/Gänse Spiel/Gänse Spiel/obj/Debug/net8.0/Gänse Spiel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gänse Spiel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+417c5f1a13d5dfb48b092c5b47cad63d062203e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d2ce2e65956e0ff950014c8acd12e16cf04ad2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gänse Spiel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gänse Spiel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
