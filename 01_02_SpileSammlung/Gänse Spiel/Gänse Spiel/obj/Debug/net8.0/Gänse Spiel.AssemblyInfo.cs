76ba984cd4fbb22611bb046715319c16ede1c871
JestingDart4369 <eric.haeusler@gmx.ch>
2026-03-27 11:29:35 +0100

    Update assembly info and cache files; modify Lernperiode 3 documentation with new tasks and reflections

--- 01_02_SpileSammlung/Gänse Spiel/Gänse Spiel/obj/Debug/net8.0/Gänse Spiel.AssemblyInfo.cs
+++ 01_02_SpileSammlung/Gänse Spiel/Gänse Spiel/obj/Debug/net8.0/Gänse Spiel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gänse Spiel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d2ce2e65956e0ff950014c8acd12e16cf04ad2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d9d5764f12d4752fd660f4ee95ae9ae53a0c3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gänse Spiel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gänse Spiel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 01_02_SpileSammlung/Gänse Spiel/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
+++ 01_02_SpileSammlung/Gänse Spiel/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d2ce2e65956e0ff950014c8acd12e16cf04ad2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d9d5764f12d4752fd660f4ee95ae9ae53a0c3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
